b9b1538aad97ff7542ed3d8d2f7f78b369f000c2
Papp Gergely <pappgergo096@gmail.com>
2018-07-04 15:28:08 +0200

    Item class

--- src/main/java/com/codecool/webshop/Item.java
+++ src/main/java/com/codecool/webshop/Item.java
@@ -1,4 +1,14 @@
 package com.codecool.webshop;
 
 public class Item {
+    private static int count = 0;
+    private int id;
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.id  = count++;
+    }
 }
